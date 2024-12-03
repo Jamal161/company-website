5fd4b52bdffaaa66a61597bc780fb3d288df2f43
Md Jamal Uddin <43787665+Jamal161@users.noreply.github.com>
2024-12-03 11:31:35 +0600

    Update public bool InsertT13005(T13005Data data.php

--- public bool InsertT13005(T13005Data data.php
+++ public bool InsertT13005(T13005Data data.php
@@ -1,16 +1,1 @@
-public bool InsertT13005(T13005Data data, string userCode)
-{
-    return Command(@"
-      INSERT INTO T13005 
-      (T_ENTRY_USER, T_ENTRY_DATE, T_SPECIMEN_CODE, T_LANG1_NAME, T_LANG2_NAME, T_ANALYZER_CODE, T_SPECIMEN_IMAGE) 
-      VALUES (:T_ENTRY_USER, TRUNC(SYSDATE), :T_SPECIMEN_CODE, :T_LANG1_NAME, :T_LANG2_NAME, :T_ANALYZER_CODE, :T_SPECIMEN_IMAGE)",
-        new
-        {
-            T_ENTRY_USER = userCode,
-            data.T_SPECIMEN_CODE,
-            data.T_LANG1_NAME,
-            data.T_LANG2_NAME,
-            data.T_ANALYZER_CODE,
-            data.T_SPECIMEN_IMAGE
-        });
-}
+
